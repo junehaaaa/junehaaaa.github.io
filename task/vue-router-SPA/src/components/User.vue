605785840f81084a51ab8b45e6d4f87c5002f2e7
Juneha Jang <junehaaaa@gmail.com>
2017-04-04 02:26:55 +0900

    add vue-router & vue-transtion study

---
+++ task/vue-router-SPA/src/components/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>User Page</h2>
+    <ul>
+      <li>Lorem ipsum dolor.</li>
+      <li>Lorem ipsum dolor.</li>
+      <li>Lorem ipsum dolor.</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="sass">
+  
+</style>
---
+++ task/vue-router-SPA/src/routes.js
@@ -0,0 +1,10 @@
+// Load Component
+import Home from './components/Home.vue';
+import User from './components/User.vue';
+
+// Route Setting
+export const routes = [
+  { path: '', component: Home },
+  { path: '/user', component: User },
+
+]; 
