1447d8683b4b5375c770e9e47a3b2a282d9242e2
Juneha Jang <junehaaaa@gmail.com>
2017-04-04 02:53:01 +0900

    app.vue 수정 & Navigation.vue 추가

--- task/vue-router-SPA/src/App.vue
+++ task/vue-router-SPA/src/App.vue
@@ -1,45 +1,50 @@
 <template>
   <div id="app">
     <h1>SPA</h1>
+    <app-nav></app-nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navigation from './components/Navigation.vue';
 export default {
   name: 'app',
+  components: {
+    appNav: Navigation
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app 
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #2c3e50
+  margin-top: 60px
 
-h1, h2 {
-  font-weight: normal;
-}
+h1, h2 
+  font-weight: normal
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+ul 
+  list-style-type: none
+  padding: 0
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+li 
+  display: inline-block
+  margin: 0 10px
 
-a {
-  color: #42b983;
-}
+a 
+  color: #42b983
+
+.router-link-active,
+.current-page
+  text-decoration: none
+  font-weight: 800
+
 </style>
---
+++ task/vue-router-SPA/src/components/Navigation.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="gnb">
+    <ul>
+      <!--<li><router-link to="/" active-class="current-page" exact>Home</router-link></li>
+      <li><router-link to="/user" active-class="current-page">User</router-link></li>-->
+      <router-link to="/" tag="li" exact active-class="current-page"><a>Home</a></router-link>
+      <router-link to="/user" tag="li" active-class="current-page"><a>User</a></router-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="sass">
+  
+</style>
