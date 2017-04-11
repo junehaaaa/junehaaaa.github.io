04e90fac62392cd4751abb58406240b7a1b53d3f
Juneha Jang <junehaaaa@gmail.com>
2017-04-11 18:41:52 +0900

    firebase에 dummy data 생성할수 있도록 수정

--- task/sign-up-study/src/App.vue
+++ task/sign-up-study/src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
     <h1></h1>
-      <h2>회원가입</h2>
+      <h2>더미데이터 생성기</h2>
     <div class="user-info">
       <input type="text" v-model="user_info.email" placeholder="이메일을 입력하세요">
       <input type="password" v-model="user_info.password" placeholder="패스워드를 입력하세요">
       <input type="text" v-model="user_info.nickname" placeholder="닉네임을 입력하세요">
       <input type="text" v-model="user_info.age" placeholder="나이를 입력하세요">
       <input type="tetx" v-model="user_info.gender" placeholder="성별을 입력하세요">
+      <button type="button" @click="submitData">Submit</button>
+    </div>
+    <div class="playlist_info">
+      <input type="text" v-model="playlist_info.list_new_title" placeholder="리스트 제목">
+      <input type="text" v-model="playlist_info.user_name" placeholder="유저네임">
+      <input type="text" v-model="playlist_info.cover" placeholder="커버 이미지">
+      <input type="text" v-model="playlist_info.list_track_number" placeholder="트랙 넘버">
+      <input type="text" v-model="playlist_info.alt" placeholder="이미지 설명">
       <button type="button" @click="submitData">Submit</button>
     </div>
   </div>
@@ -27,23 +35,32 @@ export default {
         age: null,
         gender: null
       },
+
+      playlist_info: {
+        list_new_title: '',
+        user_name: '',
+        list_track_number: '',
+        cover: '',
+        alt: ''
+      },
+
       datalist: [],
       resource: {},
       uri: 'SignUp'
     }
   },
 
-  // created() {
-  //   // Resource 객체에 새롭게 정의한 사용자 정의 메서드 추가
-  //   const customActions = {
-  //     saveAlt: { url: 'alternateTodoList.json', method: 'POST' }
-  //   };
-  //   this.axios = this.$axios('{uri}.json', {}, customActions);
-  // },
+  created() {
+    // Resource 객체에 새롭게 정의한 사용자 정의 메서드 추가
+    const customActions = {
+      saveAlt: { url: 'alternateTodoList.json', method: 'POST' }
+    };
+    this.axios = this.$axios('{uri}.json', {}, customActions);
+  },
 
   methods: {
     submitData() {
-      axios.post('https://vue-http-1b8e7.firebaseio.com/SignUp.json', this.user_info);
+      axios.post('https://vue-http-1b8e7.firebaseio.com/playlist.json', this.playlist_info);
       // this.axios.saveAlt({}, this.user_info);
     }
   }
