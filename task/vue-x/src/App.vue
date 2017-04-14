e87cb5e471075f09e3930e229a05a088b461a088
Juneha Jang <junehaaaa@gmail.com>
2017-04-14 23:49:02 +0900

    vue-x 추가

---
+++ task/vue-x/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+  {{ $store.state.count }}
+  <p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </p>
+</div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  name: 'app',
+  methods: mapActions([
+    'increment',
+    'decrement'
+  ])
+}
+</script>
---
+++ task/vue-x/src/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const state = {
+  count: 0
+}
+
+const mutations = {
+  increment: state => state.count++,
+  decrement: state => state.count--
+}
+
+const methods = {
+  increment: ({ commit }) => commit('increment'),
+  decrement: ({ commit }) => commit('decrement')
+}
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  methods
+})
